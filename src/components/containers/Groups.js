c724cd31a8c4de354118d22d10a53cd5829b95d3
Ali Honarmand <ali.honarmand@gmail.com>
2019-06-03 21:35:30 -0700

    show the current groups

--- src/components/containers/Groups.js
+++ src/components/containers/Groups.js
@@ -2,12 +2,46 @@ import React, {Component} from 'react';
 import FormContainer from '../../components/containers/FormContainer';
 
 class Groups extends Component { 
+    constructor() {
+        super()
+
+        this.state = {
+            current_groups: []
+        }
+    }
+
+    componentDidMount() {
+        console.log("here")
+        fetch('http://localhost:5000/api/member/groups',
+            {
+                method: "GET",
+                headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json',
+                  'Authorization': 'Bearer ' + localStorage.getItem('token')
+                }
+            }
+        )
+        
+        .then(response => {
+            if (response.ok) {
+              response.json().then(json => {
+                console.log(json);
+                this.setState({current_groups: json.group_names })
+              });
+            }
+          }
+        )
+    }
+
+
     render(){
         return (
             <div>
                 <h1>
-                    Groups
+                    Current Groups
                 </h1>
+
                 <FormContainer 
                     name="create group" 
                     fields={["group_name"]} 
@@ -21,6 +55,8 @@ class Groups extends Component {
                     }
                     }
                 />
+
+                {this.state.current_groups ? this.state.current_groups.map(g => <h1>{g}</h1>) : <h1>hi</h1>}
             </div>
             
         )
