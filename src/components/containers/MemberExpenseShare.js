26dd2c790c7ecffea434e84cbc95bf013acedb79
Ali Honarmand <ali.honarmand@gmail.com>
2019-06-16 15:57:22 -0700

    clean up of MemberExpenseShare

--- src/components/containers/MemberExpenseShare.js
+++ src/components/containers/MemberExpenseShare.js
@@ -10,8 +10,7 @@ class MemberExpenseShare extends Component {
         //         {email: "c@d.com", name: "Cristian", expense_shares:[{expense_name: "a", expense_share: 80}, {expense_name: "b", expense_share: 30}] }
         //     ]
         this.state = {
-            expense_shares: {},
-            members_expenses: []
+            expense_shares: {}
         }
         this.handleProps(this.props.members_expenses)
     }
@@ -19,19 +18,15 @@ class MemberExpenseShare extends Component {
     componentDidUpdate(prevProps) {
         console.log("****in componenetDidUpdate")
         if (this.props != prevProps){
+            console.log("changing state");
             console.log(this.props)
             this.handleProps(this.props.members_expenses) 
         }
-
       }
 
     handleProps(members_expenses){
-        console.log(`members_expenses: ${members_expenses}`)
-        this.expense_names = this.extract_expense_names(members_expenses)
-        console.log(`expense_names: ${this.expense_names}`)
         this.setState({
-            expense_shares: this.get_expense_shares(members_expenses),
-            members_expenses: members_expenses
+            expense_shares: this.get_expense_shares(members_expenses)
         })
     }
 
@@ -39,110 +34,104 @@ class MemberExpenseShare extends Component {
         const m = {}
         for (const member of members){
             m[member.email] = {}
+            m[member.email].name = member.name
+            m[member.email].shares = {}
             for (const e of member.expense_shares){
-                m[member.email][e.expense_name] = e.expense_share
+                m[member.email].shares[e.expense_name] = e.expense_share
             }
         }
         return m
-    }
-
-    extract_expense_names(members) {
-
-
-        var expenses = new Set();
-
-        for (const member of members){
-            const member_expense_names = member.expense_shares.map(e => e.expense_name)
-            member_expense_names.forEach(expense => expenses.add(expense))
-        }
-        
-        return Array.from(expenses)
-    }
-
-    create_email_expense_map(members) {
-        const m = {}
-        for (const member of members){
-            for (const e of member.expense_shares){
-                m[this.create_email_expense_tuple(member.email, e.expense_name)] = e.expense_share
-                console.log(`size: ${m.size}`)
-            }
-        }
-        return m
-    }
-
-    get_member_share(email, expense_name){
-        if (expense_name in this.state.expense_shares[email]){
-            return this.state.expense_shares[email][expense_name]
-        } else {
-            return ""
-        }
     }
 
     handle_input(email, expense_name){
         return (event) => {
-            let new_share_value = event.target.value;
+            let new_share_value = (event.target.value*100).toString();
 
             const prev_shares = this.state.expense_shares
-            prev_shares[email][expense_name] = new_share_value
+            prev_shares[email].shares[expense_name] = new_share_value
             this.setState({expense_shares: prev_shares})
-            console.log(this.state.expense_shares)
         }
+    }
+
+    get_expense_names(){
+        console.log(`state.expense_shares`);
+        console.log(this.state.expense_shares)
+        const email1 = Object.keys(this.state.expense_shares)[0]
+        if (!email1){
+            return []
+        }
+        return Object.keys(this.state.expense_shares[email1].shares)
+    }
+
+    tableHeader(){
+        return (
+            <tr>
+                <th>name</th>
+                <th>email</th>
+                {this.get_expense_names().map(
+                    e => <th> {e} </th>
+                )}
+            </tr>
+        )
+    }
+
+    tableContent(){
+        const expense_shares = this.state.expense_shares
+        return Object.keys(expense_shares).map( email => {
+            const name = expense_shares[email].name
+            const shares = expense_shares[email].shares
+            console.log(`SHARES:`);
+            console.log(shares);
+            return (
+                <tr>
+                    <td>{ name || "" }</td>
+                    <td>{ email || "" }</td>
+
+                    {Object.keys(shares).map(expense_name =>
+                            <td>
+                                <Input inputType={'text'}
+                                    name={[email, expense_name].join('-')} 
+                                    value={parseFloat(this.state.expense_shares[email].shares[expense_name])/100}
+                                    handleChange = {this.handle_input(email, expense_name)}
+                                />
+                            </td>
+                    )}
+
+                </tr>
+            )
+        }
+        )                                
+    }
+
+    tableButtons(){
+        return (
+            <tr>
+                <td></td>
+                <td></td>
+                {this.get_expense_names().map(expense_name => 
+                    <td>
+                        <Button 
+                            action = {() => console.log("hi")}
+                            title={"change expense ratio"}
+                        />
+                    </td>
+                )}
+            </tr>
+        )
     }
 
     render() {
         return (
-        <div>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>name</th>
-                        <th>email</th>
-                        {this.expense_names.map(
-                            e => <th> {e} </th>
-                        )}
-                    </tr>
-                </tbody>
-                    
-                {
+            <div>
+                <table>
                     <tbody>
-                        {
-                        this.state.members_expenses.map(member => 
-                                <tr>
-                                    <td>{ member.name || "" }</td>
-                                    <td>{ member.email || "" }</td>
-                                    {
-                                        this.expense_names.map(expense_name =>
-                                        <td>
-                                            <Input inputType={'text'}
-                                                name={[member.email, expense_name].join('-')}
-                                                value={
-                                                    expense_name in this.state.expense_shares[member.email] ? 
-                                                    this.state.expense_shares[member.email][expense_name]/100 : 0
-                                                } 
-                                                handleChange = {this.handle_input(member.email, expense_name)}
-                                            />
-                                        </td>)
-                                    }
-                                </tr>
-                            
-                        )}
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            {this.expense_names.map(expense_name => 
-                                <td>
-                                    <Button 
-                                        action = {() => console.log("hi")}
-                                        title={"change expense ratio"}
-                                    />
-
-                                </td>
-                            )}
-                        </tr>
+                        {this.tableHeader()}
+                        {this.tableContent()}
+                        {this.tableButtons()}
                     </tbody>
-                }
+
                 </table>
-        </div>
+            </div>
         )
     }
 }
